99d0f9284700d271086bf8205abf14e62c6c6222
Ashutosh Sharma <ashutoshsharmaengg@live.com>
2025-07-25 20:20:53 -0700

    agents created

---
+++ Analyser_GPT/agents/data_analyzer_agent.py
@@ -0,0 +1,12 @@
+from autogen_agentchat.agents import AssistantAgent
+from prompts.system_messages import DATA_ANALYZER_SYSTEM_MESSAGE
+
+def getDataAnalyserAgent(model_client):
+
+    data_analyzer_agent = AssistantAgent(
+        name='Data_Analyzer_Agent',
+        model_client=model_client,
+        description='An Agent that solves the Data Analysis Problem and gives the code as well',
+        system_message=DATA_ANALYZER_SYSTEM_MESSAGE
+    )
+    return data_analyzer_agent
